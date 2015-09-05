229799acda175761ef594afbdf5a8a2b6619d453
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2015-09-05 19:12:59 +0300

    Account spec validate plutus account association

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   let(:account) { create(:account) }
   it 'should have Plutus Account assigned' do
-    binding.pry
-    expect(account.plutus_account).to be_kind_of Plutus::Account
+    expect(account.plutus_account).to be_valid
   end
 end
