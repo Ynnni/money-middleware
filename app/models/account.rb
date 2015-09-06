ce211b535a2dcb58d2351015c076c4d5d333d06e
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2015-09-06 23:54:35 +0300

    Account balance specs

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,4 +13,8 @@ class Account < ActiveRecord::Base
   has_many :revenues
 
   validates_presence_of :name
+
+  def balance
+    revenues.sum(:amount) - expenses.sum(:amount)
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -11,5 +11,14 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  let(:account) { build(:account) }
+  it 'revenue should increase account balance' do
+    revenue = build(:revenue, amount: 100, account: account)
+    expect { revenue.save }.to change { account.balance }.from(0).to(100)
+  end
 
+  it 'expense should increase account balance' do
+    expense = build(:expense, amount: 100, account: account)
+    expect { expense.save }.to change { account.balance }.from(0).to(-100)
+  end
 end
