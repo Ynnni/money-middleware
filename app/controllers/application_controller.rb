003206b331a1ada1b4741d237fb0f67e114539e4
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2015-10-30 09:03:00 +0200

    Purge all controllers, clean Gemfile

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  # rescue_from ActionController::UnknownFormat, with: :unknown_format
-
-  def index
-    render text: '', layout: 'application'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'application#index'
-  resources :accounts
-  resources :expenses
-
   mount Money::API => '/'
 end
